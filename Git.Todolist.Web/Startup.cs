ae463613266b7c265168c933a3f5a663219decc8
greezfyc <greezfyc@163.com>
2017-08-07 16:07:18 +0800

    结构调整

---
+++ Git.Todolist.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Git.Todolist.Web.Startup))]
+namespace Git.Todolist.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
